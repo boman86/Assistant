9d216fbfda0c18c748f18dff172c34bd5ca08eb0
Robin Malfait <malfait.robin@gmail.com>
2016-01-09 21:45:43 +0100

    Cleaning

--- app/js/modules/plugins/Screen.js
+++ app/js/modules/plugins/Screen.js
@@ -1,6 +1,4 @@
 module.exports = robot => {
-    const h = robot.h
-
     robot.listen(/^clear ?(screen)?$/, "Clear the screen", res => {
         robot.clearScreen()
     })
--- dist/js/modules/plugins/Screen.js
+++ dist/js/modules/plugins/Screen.js
@@ -1,8 +1,6 @@
 "use strict";
 
 module.exports = function (robot) {
-    var h = robot.h;
-
     robot.listen(/^clear ?(screen)?$/, "Clear the screen", function (res) {
         robot.clearScreen();
     });
