86a73fca5b555309f84653fd5947c5623ce0d6fe
Robin Malfait <malfait.robin@gmail.com>
2016-01-12 21:13:42 +0100

    Cleaning the plugin.js

--- app/js/modules/Plugin.js
+++ app/js/modules/Plugin.js
@@ -4,8 +4,7 @@ class Plugin {
     constructor(name, cb) {
         this.name = name
         this.robot = new Robot()
-        this.cb = cb;
-        this.cb(this.robot)
+        cb(this.robot)
         this.commands = this.robot.commands()
     }
 
