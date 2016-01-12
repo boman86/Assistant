821a4c0ada0635af1589d11c6f044e4a9d30afba
Robin Malfait <malfait.robin@gmail.com>
2016-01-12 13:59:11 +0100

    Cards, undo, redo

--- app/js/modules/CardManager.js
+++ app/js/modules/CardManager.js
@@ -3,21 +3,58 @@ import h from "hyperscript"
 
 const OUTPUT = Symbol()
 const Cards = Symbol()
+const CardsIndex = Symbol()
 
 class CardManager {
 
-    constructor(output) {
+    constructor(output, undoEl, redoEl) {
         this[OUTPUT] = output
         this[Cards] = Immutable.List([])
+        this[Cards] = this[Cards].push(Immutable.List([]))
+        this[CardsIndex] = this[Cards].count() - 1
+
+        this.undoEl = undoEl
+        this.redoEl = redoEl
+
+        undoEl.addEventListener('click', e => this.undo())
+        redoEl.addEventListener('click', e => this.redo())
+    }
+
+    setCards(cards) {
+        this[Cards] = this[Cards].push(cards)
+        this[CardsIndex] = this[Cards].count() - 1
+        this.draw()
     }
 
     spawn(hObj) {
-        this[OUTPUT].insertBefore(h('.card', hObj), this[OUTPUT].firstChild)
+        this.setCards(this[Cards].last().push(h('.card', hObj)))
     }
 
     clear() {
-        this[OUTPUT].innerHTML = ""
+        this.setCards(Immutable.List([]))
+    }
+
+    draw() {
+        this[OUTPUT].innerHTML = "";
+        this[Cards].get(this[CardsIndex]).reverse().forEach(c => this[OUTPUT].appendChild(c))
+
+
+        var hasUndo = this[CardsIndex] !== 0
+        var hasRedo = this[CardsIndex] !== this[Cards].count() - 1
+
+        this.undoEl.disabled = hasUndo ? '' : 'disabled'
+        this.redoEl.disabled = hasRedo ? '' : 'disabled'
+    }
+
+    undo() {
+        --this[CardsIndex]
+        this.draw()
+    }
+
+    redo() {
+        ++this[CardsIndex]
+        this.draw()
     }
 }
 
-module.exports = output => new CardManager(output)
+module.exports = (output, undoEl, redoEl) => new CardManager(output, undoEl, redoEl)
